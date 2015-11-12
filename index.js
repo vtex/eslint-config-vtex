2bceaeba918e9d20e237a88f42a80d015e4368ac
Breno Calazans <breno@vtex.com.br>
2015-11-12 10:15:20 -0200

    Change rules
    - Remove strict
    - Remove no-underscore-dangle
    - Remove new-cap
    - Change curly from "all" to "multi-line"
    - Add no-trailing-spaces
    - Add react/wrap-multilines
    - Add semi
    - Add no-extra-semi
    - Add camelcase
    - Add SwitchCase indent

--- index.js
+++ index.js
@@ -22,19 +22,21 @@ module.exports = {
     "templateStrings": true
   },
   "rules": {
-    "quotes": [2, "single"],
-    "strict": [2, "never"],
-    "no-underscore-dangle": [0],
-    "new-cap": [0],
-    "no-unused-vars": [1, {"vars": "all", "args": "after-used"}],
-    "curly": [2, "all"],
-    "no-else-return": [2],
     "block-spacing": [2, "always"],
     "brace-style": [2, "1tbs", { "allowSingleLine": true }],
-    "indent": [2, 2],
+    "camelcase": [2, {"properties": "always"}],
+    "curly": [2, "multi-line"],
+    "indent": [2, 2, { "SwitchCase": 1 }],
     "key-spacing": [2, {"beforeColon": false, "afterColon": true}],
-    "space-before-keywords": [2, "always"],
-    "space-after-keywords": [2]
+    "no-else-return": [2],
+    "no-extra-semi": [2],
+    "no-trailing-spaces": [2],
+    "no-unused-vars": [1, {"vars": "all", "args": "after-used"}],
+    "quotes": [2, "single"],
+    "react/wrap-multilines": 1,
+    "semi": [2, "always"],
+    "space-after-keywords": [2],
+    "space-before-keywords": [2, "always"]
   },
   "plugins": [
     "react"
