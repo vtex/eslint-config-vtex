209dfa7bbc37a1310e75166af6b28066f0a276c0
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-02-06 13:56:00 -0200

    Add eslint recommended rules

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 module.exports = {
-  "extends": [
+  extends: [
     "plugin:import/recommended",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
+    "eslint:recommended",
   ],
-  "parser": "babel-eslint",
-  "plugins": ["import", "lodash"],
-  "rules": {
+  parser: "babel-eslint",
+  plugins: ["import", "lodash"],
+  rules: {
     "import/no-unresolved": [
       "error",
       {
-        "commonjs": true,
-        "amd": true,
-        "ignore": ["^([a-zA-Z@]+[-\\.]?)+"]
-      }
+        commonjs: true,
+        amd: true,
+        ignore: ["^([a-zA-Z@]+[-\\.]?)+"],
+      },
     ],
     "lodash/import-scope": [2, "method"],
   },
-  "env": {
-    "browser": true,
-    "node": true,
-    "mocha": true,
-    "jest": true
+  env: {
+    browser: true,
+    node: true,
+    mocha: true,
+    jest: true,
+    es6: true,
   },
-  "globals": {
-    "__DEV__": true
-  }
-};
+  globals: {
+    __DEV__: true
+  },
+}
